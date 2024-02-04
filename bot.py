8d54255367f896ac001e9164749745f90d6e122d
j-0928 <j928yy@gmail.com>
2024-02-03 16:38:46 -0800

    assistant working
    AI healthcare assistant works on custom dataset

--- bot.py
+++ bot.py
@@ -1,25 +1,41 @@
-import os
+import os, time, json
 from openai import OpenAI
-client = OpenAI(
-    api_key=os.environ.get("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")
+def pretty_print(messages):
+    print("# Messages")
+    for m in messages:
+        print(f"{m.role}: {m.content[0].text.value}")
+    print()
+assistant_id = "asst_waK5VGLuZif0v7F2NV5YLB87"
+client = OpenAI(api_key=("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"))
+def submit_message(assistant_id, thread, user_message):
+    client.beta.threads.messages.create(
+        thread_id=thread.id, role="user", content=user_message
+    )
+    return client.beta.threads.runs.create(
+        thread_id=thread.id,
+        assistant_id=assistant_id,
+    )
+def get_response(thread):
+    return client.beta.threads.messages.list(thread_id=thread.id, order="asc")
+
+def wait_on_run(run, thread):
+    while run.status == "queued" or run.status == "in_progress":
+        run = client.beta.threads.runs.retrieve(
+            thread_id=thread.id,
+            run_id=run.id,
+        )
+        time.sleep(0.5)
+    return run
+def create_thread_and_run(user_input):
+    thread = client.beta.threads.create()
+    run = submit_message(assistant_id, thread, user_input)
+    return thread, run
+
+
+# Emulating concurrent user requests
+inputstring = input("Enter symptoms: ")
+thread1, run1 = create_thread_and_run(
+    inputstring
 )
-file = client.files.create(
-    file = open("data.pdf", "rb"),
-    purpose="assistants"
-)
-assistant = client.beta.assistants.create(
-  name = "Heal.io - BellHacks",
-  description = "You are a helpful assistant who will give a diagnosis based on a user's symptoms given and also return possible drugs needed with a total itemized price for the drugs from the data.pdf by taking into consideration on average units per package and using that information to return an accurate price. Only return a diagnosis based on the symptoms within 15 words and only return the drugs included in the total cost and total cost itself. Omit all calculations only including the actual end result of the calculation. Include the total calculations for the cost of the drugs and the diagnosis all in one answer instead of dividing the it into 2 parts. Omit the cost per tablet and instead only return the total cost and include how many tablets come in the pack of the total cost. Make sure the total number of words is under 35 words.",
-  model = "gpt-4-turbo-preview",
-  tools = [{"type": "retrieval"}],
-  file_ids = [file.id]
-)
-thread = client.beta.threads.create(
-  messages = [
-    {
-      "role": "user",
-      "content": "I feel tired, I have a cough, and I have a loss of apetite. What disease do I have and what drugs do I need and how much do they cost?",
-      "file_ids": [file.id]
-    }
-  ]
-)
+run1 = wait_on_run(run1, thread1)
+pretty_print(get_response(thread1))
